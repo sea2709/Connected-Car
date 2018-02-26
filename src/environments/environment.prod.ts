20545a7bf059a62b1be26b4c147ad0b391a39e15
Hai-Dang Tran <haidang.tran@viivue.com>
2018-02-25 20:09:20 -0600

    update base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
     api: {
         /* tslint:disable */
         jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOiIyMDE4LTAyLTEyVDE2OjA0OjU3LjAyNloiLCJpYXQiOjE1MTg0NTE0OTcsImV4cCI6MTUyNzA5MTQ5N30.9aSdKxZIXtR4VKMwriBUgc_fbikSXhLzG0nE3RMgNrI',
-        baseURL: 'https://conn-car-tcm-hdang.herokuapp.com/login',
+        baseURL: 'https://conn-car-tcm-hdang.herokuapp.com',
         customer: '/api/v1/Customer/Lookup',
         sim: '/api/v1/SIM/Lookup',
         createPin: '/api/v1/PIN/Create',
